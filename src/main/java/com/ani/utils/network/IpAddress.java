512380754ca805f481bb22f141a66c7155392028
zsl <zsl_hit@qq.com>
2016-11-11 16:44:38 +0800

    add byte[] getIPAddress

--- src/main/java/com/ani/utils/network/IpAddress.java
+++ src/main/java/com/ani/utils/network/IpAddress.java
@@ -7,8 +7,9 @@ import java.util.Enumeration;
  * Created by zsl on 16-11-10.
  */
 public class IpAddress {
-    public static String getIpAddress() throws UnknownHostException, SocketException {
+    public static String getIpAddress() {
         String ip = "";
+        String NETWORK_WIRE = "wlp3s0";
         String NETWORK_CARD = "enp0s25";
         String NETWORK_CARD_BAND = "bond0";
         try {
@@ -16,7 +17,8 @@ public class IpAddress {
             while (e1.hasMoreElements()) {
                 NetworkInterface ni = e1.nextElement();
                 //单网卡或者绑定双网卡
-                if ((NETWORK_CARD.equals(ni.getName())) || (NETWORK_CARD_BAND.equals(ni.getName()))) {
+                //TODO 不同系统的适配问题
+                if ((NETWORK_CARD.equals(ni.getName())) || (NETWORK_CARD_BAND.equals(ni.getName())) || NETWORK_WIRE.equals(ni.getName())) {
                     Enumeration<InetAddress> e2 = ni.getInetAddresses();
                     while (e2.hasMoreElements()) {
                         InetAddress ia = e2.nextElement();
@@ -32,5 +34,9 @@ public class IpAddress {
             System.out.println("IpGetter.getLocalIP出现异常！异常信息：" + e.getMessage());
         }
         return ip;
+    }
+
+    public static byte[] getAddress() throws UnknownHostException {
+        return InetAddress.getByName(getIpAddress()).getAddress();
     }
 }
