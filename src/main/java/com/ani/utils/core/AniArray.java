f20e046627ad1e9a8e98bb1c4a1e67537b154ac0
zhanglina <xiaojiang1129@github.com>
2018-04-01 16:06:06 +0800

    add array copy

---
+++ src/main/java/com/ani/utils/core/AniArray.java
@@ -0,0 +1,36 @@
+package com.ani.utils.core;
+
+import java.util.Arrays;
+
+/**
+ * Created by zhanglina on 18-3-30.
+ */
+public class AniArray {
+    public static byte[] concatByteAll(byte[] first, byte[]... rest) {
+        int totalLength = first.length;
+        for (byte[] array : rest) {
+            totalLength += array.length;
+        }
+        byte[] result = Arrays.copyOf(first, totalLength);
+        int offset = first.length;
+        for (byte[] array : rest) {
+            System.arraycopy(array, 0, result, offset, array.length);
+            offset += array.length;
+        }
+        return result;
+    }
+
+    public static <T> T[] concatAll(T[] first, T[]... rest) {
+        int totalLength = first.length;
+        for (T[] array : rest) {
+            totalLength += array.length;
+        }
+        T[] result = Arrays.copyOf(first, totalLength);
+        int offset = first.length;
+        for (T[] array : rest) {
+            System.arraycopy(array, 0, result, offset, array.length);
+            offset += array.length;
+        }
+        return result;
+    }
+}
