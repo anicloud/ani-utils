4093c07e1c39b4ef28de50d178f09c4c5377cca2
ebrx <ebrx@vip.qq.com>
2018-06-19 19:57:56 +0800

    #1 imp interface

--- src/test/com/ani/test/utils/core/AniGeneralUtilsTest.java
+++ src/test/com/ani/test/utils/core/AniGeneralUtilsTest.java
@@ -2,15 +2,24 @@ package com.ani.test.utils.core;
 
 
 import com.ani.utils.core.AniGeneralUtils;
+import com.ani.utils.dto.AniPattern;
 import org.junit.Test;
 
 public class AniGeneralUtilsTest {
 
     @Test
     public void testCombineIntToLong() {
+
         System.out.println(
                 AniGeneralUtils.combineIntToLong(100, 200)
         );
+    }
+
+    @Test
+    public void testUrlRegEx() {
+        String url = "1";
+        boolean matches = url.matches(AniPattern.URL_PATTERN.getPattern());
+        System.out.println(matches);
     }
 
 }
